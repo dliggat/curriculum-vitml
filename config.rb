dff741ad2995f96f5bcb551b986962dddef62f96
Dave Liggat <dliggat@gmail.com>
2015-01-27 15:20:03 -0800

    Add foundation via bower

--- config.rb
+++ config.rb
@@ -3,9 +3,10 @@
 ###
 
 # Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
+compass_config do |config|
+  config.add_import_path "bower_components/foundation/scss"
+  config.output_style = :compact
+end
 
 ###
 # Page options, layouts, aliases and proxies
